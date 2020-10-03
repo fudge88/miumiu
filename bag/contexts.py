40d4430db5ca805f379c51d56115dd7570c22544
fudge88 <f.akhlaq@outlook.com>
2020-10-03 16:56:10 +0000

    delete function and update function added to bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,8 +20,7 @@ def bag_contents(request):
             'quantity': item_data,
             'product': product,
         })
-
-
+        
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.shopping_bag, name='shopping_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('edit/<item_id>/', views.edit_bag, name='edit_bag'),
+    path('remove/<item_id>/', views.remove_item, name='remove_item'),
 ]
