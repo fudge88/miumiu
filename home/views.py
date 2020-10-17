fccce90c465b76ef0ad7e9184e71d637579c60bd
fudge88 <f.akhlaq@outlook.com>
2020-10-17 14:57:12 +0000

    google api added- location icon now wowrks

--- home/views.py
+++ home/views.py
@@ -7,3 +7,6 @@ from django.shortcuts import render
 def index(request):
     """ view to return index page"""
     return render(request, 'home/index.html')
+
+def location(request):
+    return render(request,'home/map.html')
