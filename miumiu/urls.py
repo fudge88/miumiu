7a37aa4e5effcddaf58ad5076d42913e9f6a7dfd
fudge88 <f.akhlaq@outlook.com>
2020-10-18 12:39:57 +0000

    favicon added

--- miumiu/urls.py
+++ miumiu/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('favicon.png', RedirectView.as_view(url=staticfiles_storage.url
+                                             ('static/favicon/favicon(1).ico')))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
